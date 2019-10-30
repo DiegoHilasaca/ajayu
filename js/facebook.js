17e80a32782053d78abf98ca1491192cd879dd3e
kamibynature <53499726+kamibynature@users.noreply.github.com>
2019-10-29 21:10:33 -0500

    Add files via upload

--- js/facebook.js
+++ js/facebook.js
@@ -1,12 +1,32 @@
+/*
+------------Declaracion de variables
+-----------------Navegacion--------- */
+
 let boton_facebook=document.getElementById('facebook');
 let inicio=document.getElementById('inicio');
 let inicio_brand=document.getElementById('inicio_brand');
-
+/*-----------------Filtro por año------------ */
 let a_2016=document.getElementById('a_2016');
 let a_2017=document.getElementById('a_2017');
 let a_2018=document.getElementById('a_2018');
 let a_2019=document.getElementById('a_2019');
 
+let talleres=document.getElementById('talleres');
+let charlas=document.getElementById('charlas');
+let fogon=document.getElementById('fogon');
+let laboratorio=document.getElementById('laboratorio');
+let eventos=document.getElementById('eventos');
+let reconocimientos=document.getElementById('reconocimientos');
+let festival=document.getElementById('festival');
+
+let talleres_nav=document.getElementById('talleres_nav');
+let charlas_nav=document.getElementById('charlas_nav');
+let fogon_nav=document.getElementById('fogon_nav');
+let laboratorio_nav=document.getElementById('laboratorio_nav');
+/*let eventos=document.getElementById('eventos');
+let reconocimientos=document.getElementById('reconocimientos');*/
+
+/*-----------------Publicaciones--------- */
 
 let publicaciones=document.getElementById('publicaciones');
 let contenido=document.getElementById('contenido');
@@ -33,10 +53,12 @@ class UI{
                 .then(res=>res.json())
          
     }
+    
 }
 
-slider_inicio=()=>{
-    let url='./publicaciones.json';
+slider_inicio=(search)=>{
+    let url='./bd.json';
+    actividades.className="actividades";
     fetch(url,{
         headers:{
             'Content-Type':'application/json'
@@ -69,7 +91,7 @@ slider_inicio=()=>{
         let contador=0;
         
         for(let data of res){
-            
+            if (data.year== search) {
             template_items+=`
                 <li data-target="#carouselExampleCaptions" data-slide-to="${contador}"></li>
             `;
@@ -89,6 +111,8 @@ slider_inicio=()=>{
             if (contador>0) {
                 active='';
             }
+            }
+
             
         }
 
@@ -96,57 +120,35 @@ slider_inicio=()=>{
         let slider_items=document.getElementById('slider_items');
         slider_header.innerHTML=template;
         slider_items.innerHTML=template_items;  
-        console.log(res[0]);   
+        
     })
 }
 
 
 
 
-cargar_card=(url,tittle)=>{
-    let ui=new UI();
-    contenido.innerHTML='';
-    ui.list(url)
-    .then(res=>{
-        let template=`
-            <h3 class="cards_tittle" >Publicaciones ${tittle}</h3>
-            <div class="cards_container" id="cards_container">
-        `;
-        let template_card_publicaciones='';
-        for(let data of res){
-            template_card_publicaciones+=`
-            <div class="card_publicaciones card">
-                <img src="${data.imagen}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${data.titulo}</h5>
-                    <p class="card-text">${data.descripcion}</p>
-                    <a href="${data.link}" class="btn btn-info">Leer más</a>
-                </div>
-            </div>
-            `;
-        }
-        template+=`</div>`;
-        contenido.innerHTML=template;
-        let cards_container=document.getElementById('cards_container');
-        cards_container.innerHTML=template_card_publicaciones;
 
-    })
-}
-cargar_card_portada=()=>{
-    let tittle='Ajayu 2019';  
-    let url='./publicaciones.json';    
+
+card_categoria=(search)=>{
     let ui=new UI();
-    publicaciones.innerHTML='';
+    let url='./bd.json';
+    actividades.className="actividades_2";
+    contenido.innerHTML="";      
     ui.list(url)
+    .then(res=>res.reverse())
     .then(res=>{
         let template=`
-            <div class="cards_tittle_container">
-                <h3 class="cards_tittle" >Publicaciones ${tittle}</h3>
-            </div>
-            <div class="cards_container" id="cards_container">
-        `;
-        let template_card_publicaciones='';
-        for(let data of res){
+        <div class="cards_tittle_container">
+            <h3 class="cards_tittle" >${search.toUpperCase()}</h3>
+        </div>
+        <div class="cards_container" id="cards_container">
+    `;
+    
+    let template_card_publicaciones='';
+    for(let data of res){
+        
+        if(data.categoria == search||data.year== search){
+            
             template_card_publicaciones+=`
             <div class="card_publicaciones card">
                 <img src="${data.imagen}" class="card-img-top" alt="...">
@@ -158,48 +160,105 @@ cargar_card_portada=()=>{
             </div>
             `;
         }
-        template+=`</div>`;
-        publicaciones.innerHTML=template;
-        let cards_container=document.getElementById('cards_container');
-        cards_container.innerHTML=template_card_publicaciones;
-
+    }
+    template+=`</div>`;
+    publicaciones.innerHTML=template;
+    let cards_container=document.getElementById('cards_container');
+    cards_container.innerHTML=template_card_publicaciones;
     })
 }
 
+
 window.onload=()=>{
-    slider_inicio();
-    /*portada_imagen();*/
-    cargar_card_portada();
+    let search='2019'; 
+    slider_inicio(search);
+    /*portada_imagen(); --------------este si vale*/
+    /*cargar_card_portada();*/
+    /*card_actividades();*/
+    /*----------------Actividades que realiza ajayu----------- */
+
+
+    console.log(festival);
+    /*-----------Event Listeners------- */
+
+    
 };
+festival.addEventListener('click',()=>{    
+    let search='festival';    
+    card_categoria(search);
+    
+    })
+fogon.addEventListener('click',()=>{    
+    let search='fogon';    
+    card_categoria(search);
+})
+fogon_nav.addEventListener('click',()=>{    
+    let search='fogon';
+    card_categoria(search);
+})
+talleres.addEventListener('click',()=>{
+    let search='talleres'
+    card_categoria(search)
+})
+
+talleres_nav.addEventListener('click',()=>{
+    let search='talleres'
+    card_categoria(search)
+})
+
+charlas.addEventListener('click',()=>{
+    let search='charlas'
+    card_categoria(search)
+})
+charlas_nav.addEventListener('click',()=>{
+    let search='charlas'
+    card_categoria(search)
+})
+laboratorio.addEventListener('click',()=>{
+    let search='laboratorio'
+    card_categoria(search)
+})
+laboratorio_nav.addEventListener('click',()=>{
+    let search='laboratorio'
+    card_categoria(search)
+})
+eventos.addEventListener('click',()=>{
+    let search='eventos'
+    card_categoria(search)
+})
+reconocimientos.addEventListener('click',()=>{
+    let search='reconocimientos'
+    card_categoria(search)
+})
 inicio.addEventListener('click',()=>{
-    slider_inicio();
-    cargar_card_portada();
+    let search='2019'
+    slider_inicio(search);
+
 })
 inicio_brand.addEventListener('click',()=>{
-    slider_inicio();
-    cargar_card_portada();
+    let search='2019'
+    slider_inicio(search);
+    
+    
 })
 
 a_2016.addEventListener('click',()=>{    
-    let tittle='Ajayu 2016';
-    let url='./ajayu_2016.json';
-    cargar_card(url,tittle);
+    let search='2016'
+    card_categoria(search)
 })
 a_2017.addEventListener('click',()=>{
-    let tittle='Ajayu 2017';   
-    let url='./ajayu_2017.json';
-    cargar_card(url,tittle);    
+    let search='2017'
+    card_categoria(search)   
 })
 a_2018.addEventListener('click',()=>{
-    let tittle='Ajayu 2018';    
-    let url='./ajayu_2018.json';
-    cargar_card(url,tittle);
+    let search='2018'
+    card_categoria(search)
 })
 a_2019.addEventListener('click',()=>{ 
-    
-    slider_inicio();
-    cargar_card_portada();
+    let search='2019'
+    card_categoria(search)
 })
+
 
 
 
